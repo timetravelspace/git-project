aa7e0ca40fd570df41578dd7d71b1481e31723f0
timetravelspace <156588333+timetravelspace@users.noreply.github.com>
2024-12-12 13:32:43 +0000

    First commit, you can type literally anything in this tho, this is just a comment message

---
+++ yourcode.py
@@ -0,0 +1,9 @@
+# Let's say you already have a file, just copy its contents into this codespace here
+
+# or just make a new one and start typing
+
+# now, im gonna keep my code short, but you use your main code
+
+print("Hello, World!")
+#dont forget to save the file, lol
+#now to start our commands on git bash, check the terminal for more
